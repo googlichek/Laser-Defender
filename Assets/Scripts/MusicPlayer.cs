524537cd47ace94dd4897f7f691f1fb2bda85213
Goga Akobiya <googlichek.one@gmail.com>
2017-01-15 18:54:04 +0300

    laser-defender: Additional tuning of the UI. Made music play while in game.

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,10 +4,17 @@ public class MusicPlayer : MonoBehaviour
 {
     private static MusicPlayer _instance;
 
+    public AudioClip StartClip;
+    public AudioClip GameClip;
+    public AudioClip EndClip;
+
+    private AudioSource _music;
+
+
     // Prevents from creating extra music players.
     void Awake()
     {
-        if (_instance != null)
+        if (_instance != null && _instance != this)
         {
             Destroy(gameObject);
         }
@@ -15,6 +22,31 @@ public class MusicPlayer : MonoBehaviour
         {
             _instance = this;
             DontDestroyOnLoad(gameObject);
+            _music = GetComponent<AudioSource>();
+            _music.clip = StartClip;
+            _music.loop = true;
+            _music.Play();
         }
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        _music.Stop();
+
+        switch (level)
+        {
+            case 0:
+                _music.clip = StartClip;
+                break;
+            case 1:
+                _music.clip = GameClip;
+                break;
+            case 2:
+                _music.clip = EndClip;
+                break;
+        }
+
+        _music.loop = true;
+        _music.Play();
     }
 }
